639842080ee27dad8677c083e6c29bb40149c8f8
grahamrussell1977 <graham@inajiffymedia.com>
2021-01-02 09:39:48 +0000

    modified git ignore to stop tracking Constants.swift

---
+++ youtube-onedaybuild/ConstantsTemplate.swift
@@ -0,0 +1,23 @@
+//
+//  ConstantsTemplate.swift
+//  youtube-onedaybuild
+//
+//  Created by Graham Russell on 02/01/2021.
+//
+
+/*
+Add your own keys below and uncomment the code
+
+*/
+
+import Foundation
+
+/*
+struct Constants {
+ 
+ static var API_KEY = ""
+ static var PLAYLIST_ID = ""
+ static var API_URL = "https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet&playlistId=\(Constants.PLAYLIST_ID)&key=\(Constants.API_KEY)"
+ 
+ }
+ */
---
+++ youtube-onedaybuild/Video.swift
@@ -0,0 +1,19 @@
+//
+//  Video.swift
+//  youtube-onedaybuild
+//
+//  Created by Graham Russell on 02/01/2021.
+//
+
+import Foundation
+
+
+struct Video {
+    
+    var VideoID = ""
+    var title = ""
+    var description = ""
+    var thumbnail = ""
+    var published = ""
+    
+}
