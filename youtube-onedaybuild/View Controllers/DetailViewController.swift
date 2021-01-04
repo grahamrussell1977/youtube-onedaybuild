e1c33c487eff04e42f82a643ba193955badd41d7
grahamrussell1977 <graham@inajiffymedia.com>
2021-01-04 14:53:18 +0000

    added detailviewcontroller, segue to detail, and webview in detailviewcontroller

---
+++ youtube-onedaybuild/View Controllers/DetailViewController.swift
@@ -0,0 +1,68 @@
+//
+//  DetailViewController.swift
+//  youtube-onedaybuild
+//
+//  Created by Graham Russell on 04/01/2021.
+//
+
+import UIKit
+import WebKit
+
+class DetailViewController: UIViewController {
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    @IBOutlet weak var dateLabel: UILabel!
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    @IBOutlet weak var textView: UITextView!
+    
+    var video:Video?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        
+        // Clear the fields
+        titleLabel.text = ""
+        dateLabel.text = ""
+        textView.text = ""
+        
+        // Check if there's a video
+        guard video != nil else {
+            return
+        }
+    
+        // Create the embed URL
+        let embedUrlString = Constants.YT_EMBED_URL + video!.videoID
+        
+
+        // Load it into the webview
+        let url = URL(string: embedUrlString)
+        let request = URLRequest(url: url!)
+        webView.load(request)
+        
+        // Set the title
+        titleLabel.text = video!.title
+        
+        // Set the date
+        let df = DateFormatter()
+        df.dateFormat = "EEEE, MMM d, yyyy"
+        dateLabel.text = df.string(from: video!.published)
+        
+        // Set the description
+        textView.text = video!.description
+
+
+
+    }
+    
+
+
+}
--- youtube-onedaybuild/View Controllers/ViewController.swift
+++ youtube-onedaybuild/View Controllers/ViewController.swift
@@ -1,24 +1,24 @@
+
 //
 //  ViewController.swift
 //  youtube-onedaybuild
 //
-//  Created by Graham Russell on 02/01/2021.
+//  Created by Christopher Ching on 2020-05-18.
+//  Copyright © 2020 Christopher Ching. All rights reserved.
 //
-
 import UIKit
 
 class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate, ModelDelegate {
-   
     
-
+    
     @IBOutlet weak var tableView: UITableView!
-    
     
     var model = Model()
     var videos = [Video]()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Set itself as the datasource and the delegate
         tableView.dataSource = self
         tableView.delegate = self
@@ -26,29 +26,45 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         // Set itself as the delegate of the model
         model.delegate = self
         
-       
         model.getVideos()
+    }
+    
+
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        // Confirm that a video was selected
+        guard tableView.indexPathForSelectedRow != nil else {
+            return
+        }
+        
+        // Get a reference to the video that was tapped on
+        let selectedVideo = videos[tableView.indexPathForSelectedRow!.row]
+        
+        // Get a reference to the detail view controller
+        let detailVC = segue.destination as! DetailViewController
+        
+        // Set the video property of the detail view controller
+        detailVC.video = selectedVideo
     }
     
     // MARK: - Model Delegate Methods
     
     func videosFetched(_ videos: [Video]) {
         
-        //Set the returned videos to our video property
+        // Set the returned videos to our video property
         self.videos = videos
         
         // Refresh the tableview
         tableView.reloadData()
-        
-        
     }
     
+
     // MARK: - TableView Methods
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         
         return videos.count
-      
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -56,21 +72,15 @@ class ViewController: UIViewController, UITableViewDataSource, UITableViewDelega
         let cell = tableView.dequeueReusableCell(withIdentifier: Constants.VIDEOCELL_ID, for: indexPath) as! VideoTableViewCell
         
         // Configure the cell with the data
-        
         let video = self.videos[indexPath.row]
         
         cell.setCell(video)
         
         // Return the cell
         return cell
-        
     }
-    
+
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
-    
-    
     }
-
 }
-
